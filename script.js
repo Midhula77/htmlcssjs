ba7bbf15ca3e9a941e4a34b3fe37f4955466bc34
Midhula77 <midhulaminus@gmail.com>
2023-03-16 15:17:45 +0530

    jsf

---
+++ script.js
@@ -0,0 +1,15 @@
+// function
+// function add(a,b){
+//     let sum=a+b;
+//     console.log(sum)
+// }
+// add(2,4);
+// const add=(a,b)=>{
+//     console.log(a+b);
+// }
+// add(4,4)
+const multiply=(a,b)=>
+{
+    console.log(a*b);
+}
+multiply(4,4)
